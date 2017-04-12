46849077a0a0ce21afb724731dfb5579878104a7
lsankar4033 <lsankar4033@gmail.com>
2017-04-11 20:17:35 -0700

    Standardize time logging to UTC, add comments

--- emacs/looking_glass/looking_glass.el
+++ emacs/looking_glass/looking_glass.el
@@ -4,28 +4,36 @@
 (require 'cl-lib)
 
 ;;; Logging hooks
+
 (define-minor-mode global-looking-glass-mode
   "Global minor mode for looking-glass loggin."
   nil nil nil
   (when global-looking-glass-mode
-      (progn (add-hook 'pre-command-hook 'log-keys)
-             (add-hook 'kill-emacs-hook 'save-log-buffer))))
+    (progn
+      ;; TODO figure out how to not log repeat key-presses due to a held key
+      (add-hook 'pre-command-hook 'log-keys)
+
+      ;; TODO figure out a more frequent hook that can be used to trigger 'save-log-buffer
+      (add-hook 'kill-emacs-hook 'save-log-buffer))))
 
 ;;; Logging code
 
-;; TODO buffer-dir should be configurable
+;; TODO should be configurable and synced with CLI
 (defconst keylog-log-buffer-dir "~/.emacs.d/looking_glass/")
-(defconst log-file-date-format "%Y-%m-%d %H:%M:%S")
+
+;; NOTE this works because the python dateutil.parse method handles it automatically. be careful if changing this!
+(defconst log-file-datetime-format "%Y-%m-%d %H:%M:%S %Z")
 
 ;; NOTE this is currently created at load time of *this* file
-(defvar keylog-log-buffer-name (format-time-string log-file-date-format (current-time)))
+(defvar keylog-log-buffer-name (format-time-string log-file-datetime-format (current-time) "UTC0"))
 
 (defun log-keys ()
   (interactive)
   (let ((deactivate-mark deactivate-mark))
     (when (this-command-keys)
       (let ((major-mode-name major-mode)
-	    (tt (format-time-string "%s" (current-time))))
+            ;; TODO properly handle daylight savings time. I think this creates timestamps 1 hour late
+	    (tt (format-time-string "%s" (current-time) "UTC0")))
 	(with-current-buffer (get-buffer-create keylog-log-buffer-name)
 	  (goto-char (point-max))
 	  (if (eql this-command 'self-insert-command)
